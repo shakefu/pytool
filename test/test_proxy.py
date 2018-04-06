9ac1d446d79739d8ac2d33055f40c607e93c4ca0
Jacob Alheid <shakefu@gmail.com>
2018-04-06 13:55:17 -0700

    test: Codestyle.

--- test/test_proxy.py
+++ test/test_proxy.py
@@ -4,57 +4,62 @@ import pytool
 from .util import eq_, ok_, raises, SkipTest
 
 
+def cmp(a, b):
+    # This is missing from Python 3
+    return (a > b) - (a < b)
+
+
 def test_list_proxy_instantiates_ok():
     a_list = []
     pytool.proxy.ListProxy(a_list)
 
 
 def test_list_proxy_proxies_repr():
-    l = [1, 2]
-    p = pytool.proxy.ListProxy(l)
-    eq_(repr(l), repr(p))
+    c = [1, 2]
+    p = pytool.proxy.ListProxy(c)
+    eq_(repr(c), repr(p))
 
 
 def test_list_proxy_proxies_comparisons():
-    l = [1, 2]
+    c = [1, 2]
     a = [1, 2]
     b = [3, 4]
-    p = pytool.proxy.ListProxy(l)
-    eq_(l == a, p == a)
-    eq_(l == b, p == b)
-    eq_(l <= a, p <= a)
-    eq_(l <= b, p <= b)
-    eq_(l >= a, p >= a)
-    eq_(l >= b, p >= b)
-    eq_(l < a, p < a)
-    eq_(l < b, p < b)
-    eq_(l > a, p > a)
-    eq_(l > b, p > b)
-    eq_(l != a, p != a)
-    eq_(l != b, p != b)
+    p = pytool.proxy.ListProxy(c)
+    eq_(c == a, p == a)
+    eq_(c == b, p == b)
+    eq_(c <= a, p <= a)
+    eq_(c <= b, p <= b)
+    eq_(c >= a, p >= a)
+    eq_(c >= b, p >= b)
+    eq_(c < a, p < a)
+    eq_(c < b, p < b)
+    eq_(c > a, p > a)
+    eq_(c > b, p > b)
+    eq_(c != a, p != a)
+    eq_(c != b, p != b)
 
 
 def test_list_proxy_comparison_operator():
     if six.PY3:
         raise SkipTest('Python 2')
-    l = [1, 2]
+    c = [1, 2]
     a = [1, 2]
     b = [3, 4]
-    p = pytool.proxy.ListProxy(l)
-    eq_(cmp(l, a), cmp(p, a))
-    eq_(cmp(l, b), cmp(p, b))
-    eq_(cmp(l, 'foo'), cmp(p, 'foo'))
+    p = pytool.proxy.ListProxy(c)
+    eq_(cmp(c, a), cmp(p, a))
+    eq_(cmp(c, b), cmp(p, b))
+    eq_(cmp(c, 'foo'), cmp(p, 'foo'))
     eq_(cmp(p, a), p.__cmp__(a))
 
 
 def test_list_proxy_comparison_operator_again():
-    l = [1, 2]
+    c = [1, 2]
     a = [1, 2]
     b = [3, 4]
-    p = pytool.proxy.ListProxy(l)
-    eq_(l, a)
+    p = pytool.proxy.ListProxy(c)
+    eq_(c, a)
     eq_(p, a)
-    eq_(p, l)
+    eq_(p, c)
     eq_(p, p)
     ok_(p == p)
     ok_(not p == b)
@@ -63,53 +68,53 @@ def test_list_proxy_comparison_operator_again():
 
 
 def test_list_proxy_contains_operator():
-    l = [1, 2]
-    p = pytool.proxy.ListProxy(l)
+    c = [1, 2]
+    p = pytool.proxy.ListProxy(c)
     for i in range(4):
-        eq_(i in l, i in p)
+        eq_(i in c, i in p)
 
 
 def test_list_proxy_length_operator():
-    l = [1, 2]
-    p = pytool.proxy.ListProxy(l)
-    eq_(len(l), len(p))
+    c = [1, 2]
+    p = pytool.proxy.ListProxy(c)
+    eq_(len(c), len(p))
 
 
 def test_list_proxy_set_get_and_delete_items():
-    l = [1, 2]
-    p = pytool.proxy.ListProxy(l)
-    eq_(l[0], p[0])
-    eq_(l[1], p[1])
+    c = [1, 2]
+    p = pytool.proxy.ListProxy(c)
+    eq_(c[0], p[0])
+    eq_(c[1], p[1])
     p[0] = 3
-    eq_(l, p)
-    eq_(l[0], 3)
+    eq_(c, p)
+    eq_(c[0], 3)
     del p[0]
-    eq_(l, p)
-    eq_(l[0], 2)
+    eq_(c, p)
+    eq_(c[0], 2)
 
 
 def test_list_proxy_slicing():
-    l = [i for i in range(5)]
-    p = pytool.proxy.ListProxy(l)
+    c = [i for i in range(5)]
+    p = pytool.proxy.ListProxy(c)
     s = p[1:3]
     eq_(s, [1, 2])
     ok_(isinstance(s, list))
     p[1:3] = [5, 10]
-    eq_(p, l)
-    eq_(l[1:3], [5, 10])
+    eq_(p, c)
+    eq_(c[1:3], [5, 10])
     p[1:3] = p
-    eq_(p, l)
+    eq_(p, c)
     p[1:3] = range(5)
-    eq_(p, l)
+    eq_(p, c)
     eq_(p[1:5], [0, 1, 2, 3])
     del p[1:]
-    eq_(p, l)
+    eq_(p, c)
     eq_(p, [0])
 
 
 def test_list_proxy_addition():
-    l = [1, 2]
-    p = pytool.proxy.ListProxy(l)
+    c = [1, 2]
+    p = pytool.proxy.ListProxy(c)
     n = p + p
     eq_(n, [1, 2, 1, 2])
     ok_(isinstance(n, list))
@@ -134,43 +139,43 @@ def test_list_proxy_addition():
     eq_(p, [1, 2, 3, 4, 0, 1])
     p += p
     eq_(p, [1, 2, 3, 4, 0, 1, 1, 2, 3, 4, 0, 1])
-    eq_(l, p)
+    eq_(c, p)
 
 
 def test_list_proxy_as_json():
-    l = pytool.proxy.ListProxy([])
-    l.append('foo')
-    eq_(pytool.json.as_json(l), '["foo"]')
+    c = pytool.proxy.ListProxy([])
+    c.append('foo')
+    eq_(pytool.json.as_json(c), '["foo"]')
 
 
 def test_list_proxy_multiplication():
-    l = [1, 2]
-    p = pytool.proxy.ListProxy(l)
+    c = [1, 2]
+    p = pytool.proxy.ListProxy(c)
     n = p * 2
-    eq_(n, l * 2)
+    eq_(n, c * 2)
     eq_(n, [1, 2, 1, 2])
     p *= 2
-    eq_(p, l)
+    eq_(p, c)
     eq_(p, [1, 2, 1, 2])
 
 
 def test_list_proxy_mutable_methods():
-    l = [1, 2]
-    p = pytool.proxy.ListProxy(l)
+    c = [1, 2]
+    p = pytool.proxy.ListProxy(c)
     p.append(3)
-    eq_(l, p)
+    eq_(c, p)
     eq_(p, [1, 2, 3])
     p.insert(0, 0)
-    eq_(l, p)
+    eq_(c, p)
     eq_(p, [0, 1, 2, 3])
     p.pop()
-    eq_(l, p)
+    eq_(c, p)
     eq_(p, [0, 1, 2])
     p.extend(p)
-    eq_(l, p)
+    eq_(c, p)
     eq_(p, [0, 1, 2, 0, 1, 2])
     p.extend([1, 2])
-    eq_(l, p)
+    eq_(c, p)
     eq_(p, [0, 1, 2, 0, 1, 2, 1, 2])
 
 
@@ -264,9 +269,11 @@ def test_dict_proxy_update():
     p.update([('four', 4)])
     eq_(d, p)
     eq_(p, {'one': 1, 'two': 2, 'three': 3, 'four': 4})
+
     class Items(object):
         def items(self):
             return [('five', 5)]
+
     p.update(Items())
     eq_(d, p)
     eq_(p, {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5})
@@ -311,4 +318,3 @@ def test_dict_proxy_as_json():
 def test_dict_proxy_raises_key_error():
     d = pytool.proxy.DictProxy({})
     d['foo']
-
