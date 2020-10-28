74aa919d28b192502d1ac20b0ab2b10f6b9c3607
Jacob Alheid <shakefu@gmail.com>
2020-10-27 22:15:27 -0700

    setup: Use pytest as main test runner.

--- setup.py
+++ setup.py
@@ -19,34 +19,35 @@ def version():
 
 
 setup(
-        name='pytool',
-        version=version(),
-        author="Jacob Alheid",
-        author_email="jake@about.me",
-        description="A Collection of Python Tools",
-        long_description=readme(),
-        url='http://github.com/shakefu/pytool',
-        license='ALv2',
-        packages=find_packages(exclude=['test']),
-        install_requires=['simplejson >= 3.2.0', 'six'],
-        test_suite='nose.collector',
-        tests_require=[
-            'nose',
-            'mock',
-            ],
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: Apache Software License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: Implementation :: PyPy',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Utilities',
-            ]
-        )
+    name='pytool',
+    version=version(),
+    author="Jacob Alheid",
+    author_email="jake@about.me",
+    description="A Collection of Python Tools",
+    long_description=readme(),
+    url='http://github.com/shakefu/pytool',
+    license='ALv2',
+    packages=find_packages(exclude=['test']),
+    install_requires=['simplejson >= 3.2.0', 'six'],
+    test_suite='nose.collector',
+    tests_require=[
+        'pytest',
+        'nose',  # This needs to be removed after converting test code
+        'mock',
+        ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        ]
+    )
